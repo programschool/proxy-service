5560f68ce20e5dd8a04e5e928dacd902dd58fea9
foxsir <838394225@qq.com>
2021-03-07 14:14:36 +0800

    update

--- build.sh
+++ build.sh
@@ -6,21 +6,21 @@ export GOARCH=amd64
 cd entry-proxy
 go fmt main.go
 go build main.go
-scp main root@192.168.10.105:/home/services/entry-proxy
+scp main root@192.168.50.105:/home/services/entry-proxy
 rm main
 cd ..
 
 cd middle-proxy
 go fmt main.go
 go build main.go
-scp main root@192.168.10.104:/home/services/middle-proxy
+scp main root@192.168.50.104:/home/services/middle-proxy
 rm main
 cd ..
 
 cd docker-service
 go fmt main.go
 go build main.go
-scp main root@192.168.10.103:/home/services/docker-service
+scp main root@192.168.50.103:/home/services/docker-service
 rm main
 cd ..
 
